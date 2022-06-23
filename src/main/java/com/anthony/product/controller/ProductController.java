e70976cdd6b5e210479d5a3a1290ccbeb9e2481b
Anthony Puitiza <Anthony.Puitiza@nautilus.team>
2022-06-23 10:07:32 -0500

    another way to implement example for badRequest

--- src/main/java/com/anthony/product/controller/ProductController.java
+++ src/main/java/com/anthony/product/controller/ProductController.java
@@ -2,7 +2,7 @@ package com.anthony.product.controller;
 
 import com.anthony.product.model.dto.ProductDto;
 import com.anthony.product.model.entity.ProductEntity;
-import com.anthony.product.model.exception.GlobalErrorResponse;
+import com.anthony.product.model.templateDto.templateDto.SizeResponse;
 import com.anthony.product.service.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -17,14 +17,36 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/product")
-//@Tag(name = "contact", description = "the Contact API")
 public record ProductController(ProductService productService) {
 
     @Operation(summary = "Find Product by id")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successful"),
-            @ApiResponse(responseCode = "404",
-                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = GlobalErrorResponse.class))})
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = SizeResponse.class))
+
+                    /* content = @Content(
+                             mediaType = MediaType.APPLICATION_JSON_VALUE,
+                             schema = @Schema(implementation = GlobalErrorResponse.class),
+                             examples = @ExampleObject(
+                                     name = "service-400",
+                                     summary = "400 from the service directly",
+                                     value = """
+                                             {
+                                                 "status": 404,
+                                                 "timestamp": "20-06-2022 11:17:09 PET",
+                                                 "errorCode": "PC-0001",
+                                                 "message": "Item with id {0} not found",
+                                                 "stackTrace": [
+                                                     "com.anthony.product.service.ProductService.lambda$getProduct$0(ProductService.java:24)",
+                                                     "java.base/java.util.Optional.orElseThrow(Optional.java:403)",
+                                                     "com.anthony.product.service.ProductService.getProduct(ProductService.java:22)",
+                                                     "com.anthony.product.controller.ProductController.getProduct(ProductController.java:38)"
+                                                 ],
+                                                 "errors": null,
+                                                 "debugMessage": null
+                                             }""")
+                     )*/
+            )
     })
     @GetMapping(path = "/{id}", produces = "application/json")
     public ProductEntity getProduct(@RequestHeader(name = "accept-language", required = false) String locale,
@@ -34,10 +56,10 @@ public record ProductController(ProductService productService) {
 
     @Operation(summary = "Get list of Students in the System ", tags = "getStudents")
     @ApiResponses(value = {
-            @ApiResponse(responseCode =  "200", description = "Success|OK"),
+            @ApiResponse(responseCode = "200", description = "Success|OK"),
             //@ApiResponse(responseCode = "401", description = "not authorized!"),
-           // @ApiResponse(responseCode = "403", description = "forbidden!!!"),
-            @ApiResponse(responseCode = "404", description = "not found!!!") })
+            // @ApiResponse(responseCode = "403", description = "forbidden!!!"),
+            @ApiResponse(responseCode = "404", description = "not found!!!")})
     @GetMapping(path = "/by-name/{name}", produces = "application/json")
     public ProductEntity getProductByName(@RequestHeader(name = "accept-language", required = false) Locale locale,
                                           @PathVariable String name) {
