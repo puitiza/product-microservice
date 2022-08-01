73af815920aa44eea6f818d159fa34d49dae147c
anthonypuitiza <anthony.puitiza.02@gmail.com>
2022-07-31 23:04:40 -0500

    add JWT to swagger

--- src/main/java/com/anthony/product/configuration/openApi/OpenApiConfig.java
+++ src/main/java/com/anthony/product/configuration/openApi/OpenApiConfig.java
@@ -17,10 +17,9 @@ import java.util.List;
 @Configuration
 public class OpenApiConfig {
     //public static final String AUTHORIZATION_HEADER = "Authorization";
-    /*
     @Bean
     public OpenAPI openApi() {
-        final String securitySchemeName = "Auth JWT";
+        final String securitySchemeName = "Auth-JWT";
 
         return new OpenAPI()
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
@@ -46,36 +45,6 @@ public class OpenApiConfig {
                         .termsOfService("TOC")
                         .license(new License().name("Apache 2.0").url("https://springdoc.org"))
                 );
-    }
-*/
-
-
-    @Bean
-    public OpenAPI openApi() {
-
-       /* Server localServer = new Server();
-        localServer.setDescription("local");
-        localServer.setUrl("http://localhost:8080");
-
-        Server testServer = new Server();
-        testServer.setDescription("test");
-        testServer.setUrl("https://example.org");*/
-
-        OpenAPI openAPI = new OpenAPI();
-        openAPI.info(new Info()
-                .title("Product-Microservice")
-                .description("This is a sample Spring Boot RESTFul service using springdoc-openapi and OpenAPI 3.")
-                .version("v1.0")
-                .contact(new Contact()
-                        .name("Arun")
-                        .url("https://asbnotebook.com")
-                        .email("asbnotebook@gmail.com"))
-                .termsOfService("TOC")
-                .license(new License().name("Apache 2.0").url("https://springdoc.org"))
-        );
-        //openAPI.setServers(Arrays.asList(localServer, testServer));
-
-        return openAPI;
     }
 
     protected List<Server> servers(){
